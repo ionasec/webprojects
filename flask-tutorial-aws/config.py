975b00df961a565ec9d9c06c827087d9133233fc
ionasec <ionasec@gmail.com>
2021-04-06 16:38:48 +0200

    remove

--- flask-tutorial-aws/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = b'1234'
-
-AWS_BUCKET_NAME='raz_flask_files'
-AWS_ACCESS_KEY='AKIA3DB7W7XK5DWO7DPS'
-AWS_SECRET_ACCESS_KEY='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'
-AWS_DOMAIN=http://raz_flask_files.s3.amazonaws.com/
